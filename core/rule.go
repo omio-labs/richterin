27f40f941564e4572a765b8e4908097729e77623
Jayanta Kurumathur <jayanta.k@outlook.com>
2020-07-14 10:12:09 +0200

    feat(models): Add rule model

--- core/rule.go
+++ core/rule.go
@@ -2,21 +2,21 @@ package main
 
 type Rule struct {
 	Id, Description string
-  Datasets []string
-  Transformation Transformation
-  Expectations []Expectation
-  Timeframe Timeframe
+	Datasets        []string
+	Transformation  Transformation
+	Expectations    []Expectation
+	Timeframe       Timeframe
 }
 
 type Transformation struct {
-  Type, Query string
+	Type, Query string
 }
 
 type Expectation struct {
-  Type, ColumnName, Runner, Severity string
-  ThresholdPercent int
+	Type, ColumnName, Runner, Severity string
+	ThresholdPercent                   int
 }
 
 type Timeframe struct {
-  Years, Months, Days int
+	Years, Months, Days int
 }
--- core/rule_test.go
+++ core/rule_test.go
@@ -37,8 +37,8 @@ func TestRuleUnmarshal(t *testing.T) {
 
 	assert.Equal("RULE1", rule.Id, "ID should be Unmarshaled correctly")
 	assert.Equal("Every Journey must contain a valid search", rule.Description, "Description should be Unmarshaled correctly")
-  assert.Equal([]string{"searches", "journeys"}, rule.Datasets, "Datasets should be Unmarshaled correctly")
-  assert.Equal(&Timeframe{Months: 1, Years: 0, Days: 0}, &rule.Timeframe, "Timeframe should be Unmarshaled correctly")
-  assert.Equal([]Expectation{{Type: "NOT_NULL", ColumnName: "search_id", ThresholdPercent: 90, Runner: "GREAT_EXPECTATIONS", Severity: "WARNING"}}, rule.Expectations, "Expectations should be Unmarshaled correctly")
-  assert.Equal(Transformation{Type: "SQL_QUERY", Query: "SELECT * FROM {{ journeys }} LEFT JOIN {{ searches }} where search_id = search_id"}, rule.Transformation, "Transformation should be Unmarshaled correctly")
+	assert.Equal([]string{"searches", "journeys"}, rule.Datasets, "Datasets should be Unmarshaled correctly")
+	assert.Equal(&Timeframe{Months: 1, Years: 0, Days: 0}, &rule.Timeframe, "Timeframe should be Unmarshaled correctly")
+	assert.Equal([]Expectation{{Type: "NOT_NULL", ColumnName: "search_id", ThresholdPercent: 90, Runner: "GREAT_EXPECTATIONS", Severity: "WARNING"}}, rule.Expectations, "Expectations should be Unmarshaled correctly")
+	assert.Equal(Transformation{Type: "SQL_QUERY", Query: "SELECT * FROM {{ journeys }} LEFT JOIN {{ searches }} where search_id = search_id"}, rule.Transformation, "Transformation should be Unmarshaled correctly")
 }
