a2e2ad54d201c3d33dc50cf0657af17e4aa5f10c
mayur2810 <mayur.mhi@gmail.com>
2020-08-12 11:55:32 +0200

    - added support for dataset as SQL - configuration accepted through file
    Signed-off-by: mayur2810 <mayur.mhi@gmail.com>

--- runners/great_expectations/flask_service/service.py
+++ runners/great_expectations/flask_service/service.py
@@ -1,14 +1,11 @@
-import logging
-
 from flask import request, Flask, jsonify
 
 from runners.great_expectations.job_config import JobConfigSchema, JobConfig
 from runners.great_expectations.runner import GERunner
 
 ge_runner_service = Flask(__name__)
-ge_runner_service.config["DEBUG"] = True
-
-ge_runner = GERunner()
+ge_runner_service.config.from_envvar('GE_SERVICE_SETTINGS')
+ge_runner = GERunner(ge_runner_service.config)
 
 
 @ge_runner_service.route('/runner/great-expectations/submit-job', methods=['POST'])
@@ -16,7 +13,7 @@ def submit_ge_job():
     try:
         job_config_dict = request.get_json()
         job_config: JobConfig = JobConfigSchema().load(job_config_dict)
-        logging.info("Received job config for GE: {}".format(job_config))
+        ge_runner_service.logger.info("Received job config for GE: {}".format(job_config))
         result = ge_runner.run(job_config)
     except Exception as e:
         # TODO: Return meaningful error responses
--- runners/great_expectations/runner.py
+++ runners/great_expectations/runner.py
@@ -1,6 +1,5 @@
-import os
-
 from great_expectations.dataset import SqlAlchemyDataset
+from jinja2 import Template
 from sqlalchemy import create_engine
 
 from runners.great_expectations.job_config import *
@@ -9,31 +8,51 @@ DEFAULT_GE_RESULT_FORMAT = "SUMMARY"
 
 
 class GERunner:
-    def __init__(self):
-        # TODO: Get properties through service/configuration file
-        project_id = os.getenv('GCP_PROJECT_ID')
-        temp_table_dataset = os.getenv('BIGQUERY_TEMP_TABLE_DATASET')
-        if project_id is None or temp_table_dataset is None:
-            raise Exception("Please set Env, variables")
-        self.bq_engine = create_engine('bigquery://' + project_id + '/' + temp_table_dataset)
+    def __init__(self, config: Dict[str, str]):
+        credentials_path = config.get('GOOGLE_APPLICATION_CREDENTIALS')
+        project_id = config.get('GCP_PROJECT_ID')
+        temp_table_dataset = config.get('BIGQUERY_TEMP_TABLE_DATASET')
+        if project_id is None or temp_table_dataset is None or credentials_path is None:
+            raise Exception("Invalid configuration(s) provided. Please check the configuration set-up.")
+        self.bq_engine = create_engine('bigquery://' + project_id + '/' + temp_table_dataset,
+                                       credentials_path=credentials_path)
+
+    @staticmethod
+    def __filter_dataset_instance(dataset: Dataset, dataset_type: DatasetType):
+        filtered_instances = filter(lambda instance: instance.type == dataset_type, dataset.instances)
+        if not filtered_instances:
+            raise Exception("No {} instances found".format(dataset_type))
+        return next(filtered_instances)
+
+    def __run_expectation_bigquery(self, expectation: Expectation, table_name: Optional[str] = None,
+                                   sql: Optional[str] = None):
+        if sql is None:
+            dataset = SqlAlchemyDataset(table_name=table_name, engine=self.bq_engine)
+        else:
+            dataset = SqlAlchemyDataset(custom_sql=sql, engine=self.bq_engine)
+        basic_options = {'column': expectation.column_name, 'mostly': expectation.threshold_percent * 0.01,
+                         'result_format': DEFAULT_GE_RESULT_FORMAT}
+        expectation_function = expectation.type.value
+        result = expectation_function(dataset, {**basic_options, **expectation.properties})
+        return result
 
     def run(self, config: JobConfig):
+        """
+        Executes the provided JobConfig using Great Expectations
+        :param config: JobConfig which has the Dataset(s) and its data expectation configured
+        :return: Execution results as Json
+        """
         is_sql_based = False if config.transformation is None else True
         if not is_sql_based:
             name, dataset = config.datasets.popitem()
-            bq_instances: List[BigQueryInstance] = list(
-                filter(lambda x: x.type == DatasetType.BIGQUERY, dataset.instances))
-            if not bq_instances:
-                raise Exception("No BigQuery instances found")
-            table_name = bq_instances.pop().table
-            dataset = SqlAlchemyDataset(table_name=table_name, engine=self.bq_engine)
-            expectation = config.expectation
-            basic_options = {'column': expectation.column_name, 'mostly': expectation.threshold_percent * 0.01,
-                             'result_format': DEFAULT_GE_RESULT_FORMAT}
-            expectation_function = expectation.type.value
-            result = expectation_function(dataset, {**basic_options, **expectation.properties})
-            return result.to_json_dict()
+            table_name = self.__filter_dataset_instance(dataset, DatasetType.BIGQUERY).table
+            result = self.__run_expectation_bigquery(config.expectation, table_name=table_name)
         else:
-            # TODO: SQL Execution
-            raise Exception("SQL not yet supported")
-
+            substitutions = {}
+            for name, dataset in config.datasets.items():
+                table_name = self.__filter_dataset_instance(dataset, DatasetType.BIGQUERY).table
+                substitutions[name] = table_name
+            sql_query_template = Template(config.transformation.query)
+            sql = sql_query_template.render(**substitutions)
+            result = self.__run_expectation_bigquery(config.expectation, sql=sql)
+        return result.to_json_dict()
